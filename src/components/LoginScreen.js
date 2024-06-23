ea33f2120e22f9f65a6a35d862704556c5c187e0
Merit Mohammed <cussmeritmohammed@gmail.com>
2024-06-23 03:06:18 +0100

    Committing the sstages

--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import logo from "../assets/img/health.png";
+import '@fortawesome/fontawesome-free/css/all.css'; // Import Font Awesome CSS  
+
+import "../assets/dashboard/assets/css/LoginScreen.css";
+
 const LoginScreen = () => {
   const navigate = useNavigate();
 
@@ -11,28 +16,68 @@ const LoginScreen = () => {
   };
 
   return (
-    <div className="container mt-5">
-      <div className="row justify-content-center">
-        <div className="col-md-6">
-          <div className="card">
-            <div className="card-body">
-              <h3 className="card-title text-center">Login</h3>
-              <form onSubmit={handleSubmit}>
-                <div className="form-group">
-                  <label>Email address</label>
-                  <input type="email" className="form-control" placeholder="Enter email" required />
+    <div className="container">
+      <input type="checkbox" id="flip" />
+      <div className="cover">
+        <div className="front">
+          <img src={logo} alt="logo" />
+          <div className="text">
+            <span className="text-1">Every new friend is a <br /> new adventure</span>
+            <span className="text-2">Let's get connected</span>
+          </div>
+        </div>
+        <div className="back">
+          <img src={logo} alt="logo" />
+          <div className="text">
+            <span className="text-1">Complete miles of journey <br /> with one step</span>
+            <span className="text-2">Let's get started</span>
+          </div>
+        </div>
+      </div>
+      <div className="forms">
+        <div className="form-content">
+          <div className="login-form">
+            <div className="title">Login</div>
+            <form onSubmit={handleSubmit}>
+              <div className="input-boxes">
+                <div className="input-box">
+                  <i className="fas fa-envelope"></i>
+                  <input type="text" placeholder="Enter your email" required />
                 </div>
-                <div className="form-group">
-                  <label>Password</label>
-                  <input type="password" className="form-control" placeholder="Enter password" required />
+                <div className="input-box">
+                  <i className="fas fa-lock"></i>
+                  <input type="password" placeholder="Enter your password" required />
                 </div>
-                <button type="submit" className="btn btn-primary btn-block">Login</button>
-              </form>
-              <p className="text-center mt-3">
-                Don't have an account?{' '}
-                <button className="btn btn-link" onClick={() => navigate('/register')}>Sign Up</button>
-              </p>
-            </div>
+                <div className="text"><a href="#">Forgot password?</a></div>
+                <div className="button input-box">
+                  <input type="submit" value="Submit" />
+                </div>
+                <div className="text sign-up-text">Don't have an account? <label htmlFor="flip">Signup now</label></div>
+              </div>
+            </form>
+          </div>
+          <div className="signup-form">
+            <div className="title">Signup</div>
+            <form action="#">
+              <div className="input-boxes">
+                <div className="input-box">
+                  <i className="fas fa-user"></i>
+                  <input type="text" placeholder="Enter your name" required />
+                </div>
+                <div className="input-box">
+                  <i className="fas fa-envelope"></i>
+                  <input type="text" placeholder="Enter your email" required />
+                </div>
+                <div className="input-box">
+                  <i className="fas fa-lock"></i>
+                  <input type="password" placeholder="Enter your password" required />
+                </div>
+                <div className="button input-box">
+                  <input type="submit" value="Submit" />
+                </div>
+                <div className="text sign-up-text">Already have an account? <label htmlFor="flip">Login now</label></div>
+              </div>
+            </form>
           </div>
         </div>
       </div>
